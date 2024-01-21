360c08b663b70e4f6a579f0be7dca4fe21301377
Yenzy Hebron <yshebron@up.edu.ph>
2024-01-21 18:47:40 +0800

    feat: broken down run_xval_PC2P.bat to run_PC2P.bat and eval_PC2P.bat, all to be ran from the root

--- eval_PC2P.bat
+++ eval_PC2P.bat
@@ -1,40 +1,11 @@
-@echo off
-
-set inputfile=%1%
-shift
-set outputdir=%1%
-shift
-set iters=%1%
-shift
-set mode=%1
-shift
-set pool_thresh=%1
-shift
-set num_procs=%1
-shift
-
-if "%mode%"=="" (
-	echo Mode undefined
-	exit /b
-)
-
 set maindir=%CD%
-
-echo ==========================================
-echo ================= PC2P ===================
-echo ==========================================
-echo. 
-
-:: Example:
-:: .\run_xval_PC2P.bat .\code\PC2P\Human\PIPS\PIPS_Corum_Graph.txt .\code\PC2P\results\ 10 2
-:: python code/PC2P/main.py %inputfile% %outputdir% %iters% %mode% %pool_thresh% %num_procs%
 
 echo.
 echo ==========================================
 echo ================ EVALUATING CLUSTERS =====
 echo ==========================================
 echo.
-cd %maindir%\EVALUATION
+cd %maindir%\code\PC2P\EVALUATION
 echo Evaluating PC2P...
 :: Use -n 10 cluster iterations produced in "yeast_xval_output\clusters integrated"
 :: inputdatafile = data_yeast.txt
