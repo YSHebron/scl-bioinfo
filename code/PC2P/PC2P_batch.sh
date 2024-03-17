047d4391d8a54b564c9ac2c539cb9fff827e45da
Yenzy Hebron <yshebron@up.edu.ph>
2024-03-17 15:24:47 +0800

    fix: fixed bug in code/PC2P/PC2P_batch.sh

--- code/PC2P/PC2P_batch.sh
+++ code/PC2P/PC2P_batch.sh
@@ -5,7 +5,7 @@ inputdir="code/PC2P/Yeast/FilteredPPINs"
 outputdir="code/PC2P/Results/FilteredRay"
 pool_thresh=100
 
-for $inputfile in "$inputdir"/*
+for inputfile in "$inputdir"/*
 do
     python $codepath $inputfile $outputdir -p --pool_thresh $pool_thresh
 done
