d586f341ebcdd8662f598c9ba19e6182dd9852a0
Yenzy Hebron <yshebron@up.edu.ph>
2024-06-22 12:14:47 +0800

    Wrapping-up

--- batch.sh
+++ batch.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 echo "Method, GldStd, PPIN, Predicts, Refs, Precision, Recall, F1-score, F2-score, AUC-PR, MMR, Sensitivity, PPP, Accuracy, F-Match, Separation" > results.csv
 
-for i in {1..10}
+for i in {1..1}
 do
     # metrics.sh -resultsfile -recreate
     ./metrics.sh results.csv false
